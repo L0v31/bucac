ac8737e7f40bf84d5990f4833cce4a9b93033505
L0v31 <143598931+L0v31@users.noreply.github.com>
2023-09-04 12:50:36 +0700

    Update thembulon.lua

--- thembulon.lua
+++ thembulon.lua
@@ -8,14 +8,14 @@ spawn(function()
     repeat
         task.wait()
     until game.Players.LocalPlayer and game.Players.LocalPlayer.Team ~= nil 
-    wait(1.2)
+    wait(1.5)
     require(game.ReplicatedStorage.Notification).new("<Color=Purple>Nhap type<Color=/>"):Display()
     require(game.ReplicatedStorage.Notification).new("<Color=Cyan>Abc<Color=/>"):Display()
     require(game.ReplicatedStorage.Notification).new("<Color=Black>Abc<Color=/>"):Display()
     require(game.ReplicatedStorage.Notification).new("<Color=Blue>Abc<Color=/>"):Display()
 end)
 spawn(function()
-   loadstring(game:HttpGet(
-	"https://raw.githubusercontent.com/
-   ))()
+    loadstring(game:HttpGet(
+        "https://raw.githubusercontent.com/memaybeohub/Function-Scripts/main/erth.lua"
+    ))()
 end)
